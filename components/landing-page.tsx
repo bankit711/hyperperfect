09cdb3d227479656626c14cbaf573e730ff5a99f
bankit711 <dingraham@gmail.com>
2025-03-17 13:46:31 -0700

    try

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -22,14 +22,14 @@ export default function LandingPage() {
     <div className="relative min-h-screen overflow-hidden">
       {/* Background */}
       <div className="absolute inset-0 z-0 bg-[#1a7bff]">
-        <div className="absolute inset-0 flex items-center justify-end">
+        <div className="absolute inset-0 container mx-auto px-4 md:px-6 flex items-center justify-end">
           <div
             className="w-1/2 h-full opacity-30"
             style={{
               backgroundImage:
                 "url(/images/symbol.svg)",
               backgroundSize: "60%",
-              backgroundPosition: "85% center",
+              backgroundPosition: "right center",
               backgroundRepeat: "no-repeat",
             }}
           ></div>
