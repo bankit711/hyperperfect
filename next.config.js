c6b5261fb004eb559c2bdcd5262d0be492995969
bankit711 <dingraham@gmail.com>
2025-03-17 11:12:05 -0700

    favicon

--- next.config.js
+++ next.config.js
@@ -12,17 +12,6 @@ const nextConfig = {
   // If deploying to a GitHub Pages subdomain (username.github.io/repo-name), uncomment these:
   // basePath: '/repo-name',
   // assetPrefix: '/repo-name/',
-  
-  // Export path map for static routes
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/landing-page': { page: '/landing-page' },
-    }
-  },
 }
 
 module.exports = nextConfig
